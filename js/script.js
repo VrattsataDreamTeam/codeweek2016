02c79fd46166e0e68e8fda821504ed90cb778605
Svetoslav <cvexxxxa@abv.bg>
2016-10-22 12:18:04 +0300

    new_logo
    new_lgog

--- js/script.js
+++ js/script.js
@@ -72,7 +72,7 @@ $(document).ready(function(){
     $(window).scroll(function() {
         if ($(document).scrollTop() > 50) {
             var src = "http://localhost/codeweek2016/images/logo_new2.png";
-             var src_big = "http://localhost/codeweek2016/images/logo_new.png";
+             var src_big = "http://localhost/codeweek2016/images/logo_new1.png";
             
             $("#logo").attr("src", src)
         }
