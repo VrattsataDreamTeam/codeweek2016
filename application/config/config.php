896b5943dd8c7b6065ba694a50a1e189cf10e7b9
Konvict5 <scorpioo@abv.bg>
2016-10-22 11:18:59 +0300

    new-logo-v1.0

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/codeweek2016/codeweek2016/';
+$config['base_url'] = 'http://localhost/codeweek2016/';
 
 /*
 |--------------------------------------------------------------------------
