3f2bf0998ed5abf1c9aa69ecf8bd3c0b42cad457
Konvict5 <scorpioo@abv.bg>
2016-10-22 14:10:08 +0300

    slider images and text

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -8,7 +8,7 @@
         <!-- <link rel="shortcut icon" href="images/star.png" type="favicon/ico" /> -->
 
         <title>Mamma's Kitchen</title>
-
+        <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet"> 
         <link rel="stylesheet" href="<?php echo base_url();?>css/bootstrap.min.css">
         <link rel="stylesheet" href="<?php echo base_url();?>css/font-awesome.min.css">
         <link rel="stylesheet" href="<?php echo base_url();?>css/owl.carousel.css">
