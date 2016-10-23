58d4f4d2c6c168e79da989d8cb0445d5dcf97c04
ZornitsaSD <dzorunitsa@yahoo.fr>
2016-10-23 09:38:17 +0300

    orders
    orders

--- application/views/add_order_view.php
+++ application/views/add_order_view.php
@@ -8,19 +8,19 @@ echo validation_errors();
 echo "<div>";
 echo "<h1 id='grandma'>БАБА КЕТЪРИНГ</h1>";
 echo "<h1> Искаш да опиташ вкусотийките на баба? Заповядай при нас!</h1>";
-echo form_open('login/index');
+echo form_open('Meals/save_meals#form-wrap');
 
 echo"<p>Въведи име</p>";
-echo form_input('clientname');
+echo form_input('order_name');
 
 echo"<p>Въведи телефонен номер</p>";
-echo form_input('phone');
+echo form_input('order_phone');
 
 echo"<p>Ястия</p>";//
 
-//echo "<pre>";
-//var_dump($meals);
-//echo "</pre>";
+echo "<pre>";
+var_dump($meals);
+echo "</pre>";
 
 $options_meals = array();
 
@@ -28,7 +28,7 @@ foreach ($meals as $meal) {
 	$options_meals[$meal['meal_id']] = $meal['meal_name'].' - '.$meal['meal_price']. 'лв';
 }
 
-echo form_dropdown('meals', $options_meals);
+echo form_dropdown('meal_id', $options_meals);
 
 echo"<p>Количество, бр</p>";//
 echo form_input('order_num');
@@ -38,7 +38,7 @@ echo"<p>Общо сума</p>";
 
 //$sum = $num*$meal_price;
 
-echo form_input('oreder_sum');
+echo form_input('order_sum');
 
 echo "<p>".form_submit('submit', 'Поръчай')."</p>";
 
@@ -46,3 +46,25 @@ echo form_close();
 
 
 echo "</div>";
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<div id='form-wrap'>text</div>";
